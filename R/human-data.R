10bcbe46a91064b506f5d505a0a4767cd05d094c
Gordon Custer <gcuster@uwyo.edu>
2020-11-02 13:06:32 -0700

    updated human stool data documentation

---
+++ R/human-data.R
@@ -0,0 +1,24 @@
+#' Descritpion for subset of Arabidopsis dataset used for comparison of core assignment methods
+#'
+#' Data derived from Lundberg et al. 2012. Dataset used for core assignment comparisons is a subset of the entire
+#' core Arabidopsis thaliana root microbiome dataset. We utilized only the rhizosphere samples from the M21 site
+#' for our comparisons of core assignment methods.
+#'
+#' @docType data
+#'
+#' @usage data(arabidopsis)
+#'
+#' @keywords datasets
+#'
+#' @references D.S. Lundberg, S.L. Lebeis, S.H. Paredes, S. Yourstone, J. Gehring, S. Malfatti, J. Tremblay, A. Engelbrektson,
+#' V. Kunin, T.G. del Rio, R.C. Edgar, T. Eickhorst, R.E. Ley, P. Hugenholtz, S.G. Tringe, J.L. Dangl, Defining the core
+#' Arabidopsis thaliana root microbiome, Nature. 488 (2012) 86–90. doi:10.1038/nature11237.
+#' (\href{https://www.nature.com/articles/nature11237}{Nature})
+#'
+#'What does QTL Archive thing mean?
+#' @source \href{https://www.nature.com/articles/nature11237}{Nature}
+#'
+#' @examples
+#' data(arabidopsis)
+#' core_methods(arabidopsis)
+"arabidopsis"
