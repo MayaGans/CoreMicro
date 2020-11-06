063d9c8172acd87b8565a98517707ee77a655cd3
Gordon Custer <gcuster@uwyo.edu>
2020-11-06 08:24:13 -0700

    added descriptions of both human_working_set and human_ps.

---
+++ R/human_ps.R
@@ -0,0 +1,24 @@
+#' Descritpion for human_ps dataset and human_working_set subset from the human microbiome project. This datsaet was used for comparison
+#' of core assignment methods. This phyloseq object (human_ps) contains all samples from the human microbiome project
+#' from the human microbiome project dataset. Methods for filtering of the original dataset to include only stool samples can be found in data-raw/human.R. 
+#' The entire raw human datset is named human_ps. The stool subset of the human dateset is named human_working_set. 
+#'
+#' @docType data
+#'
+#' @usage data(human_ps)
+#' @usage data(human_working_set)
+#'
+#' @keywords datasets, human microbiome, stool
+#'
+#' @references Consortium, Human Microbiome Project, and others. 2012. “Structure, Function and Diversity of the Healthy Human Microbiome.” 
+#' Nature 486 (7402). Nature Research: 207–14.
+#'
+#' @source \href{https://hmpdacc.org/ihmp/} 
+#' @source \href{http://downloads.hmpdacc.org/data/HMQCP/otu_table_psn_v35.txt.gz}
+#' @source \href{http://downloads.hmpdacc.org/data/HMQCP/v35_map_uniquebyPSN.txt.bz2}
+#'
+#' @examples
+#' data(human_ps)
+#' data(human_working_set)
+"human_ps"
+"human_working_set"
